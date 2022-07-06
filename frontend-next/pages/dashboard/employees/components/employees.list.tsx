f12c475904b1cf5f4dc50af113fa3acac161ed1f
AxRRR <alexwrot@gmail.com>
2022-07-06 10:32:41 -0500

    Pagination in all tables implement.

--- frontend-next/pages/dashboard/employees/components/employees.list.tsx
+++ frontend-next/pages/dashboard/employees/components/employees.list.tsx
@@ -1,8 +1,10 @@
+import { Paginator } from '@/components/paginator/paginator';
 import { CustomTable } from '@/components/tables/table.component';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { Employee } from '../adapters/employee.adapter';
 
 export const ListEmployees = ({ employees }: { employees: Employee[] }) => {
+  const [pagination, setPagination] = useState<number>(0);
   return (
     <Fragment>
       <CustomTable
@@ -27,24 +29,32 @@ export const ListEmployees = ({ employees }: { employees: Employee[] }) => {
           },
         ]}
       >
-        {employees.map((employee: Employee, index: number) => (
-          <tr key={index}>
-            <td>
-              <div>
-                <img src='https://imagenes.heraldo.es/files/image_654_v1/uploads/imagenes/2017/08/12/_zidanejpg_bae27af8.jpg' />{' '}
-                <p>{employee.name}</p>
-              </div>
-            </td>
-            <td>{employee.lastname}</td>
-            <td>Programador JR</td>
-            <td>16 de Mayo del 2022</td>
-            <td>${employee.payment}</td>
-            <td>
-              <button>Ver información</button>
-              <button>Editar empleado</button>
-            </td>
-          </tr>
-        ))}
+        {employees
+          .slice(pagination, pagination + 10)
+          .map((employee: Employee, index: number) => (
+            <tr key={index}>
+              <td>
+                <div>
+                  <img src='https://imagenes.heraldo.es/files/image_654_v1/uploads/imagenes/2017/08/12/_zidanejpg_bae27af8.jpg' />{' '}
+                  <p>{employee.name}</p>
+                </div>
+              </td>
+              <td>{employee.lastname}</td>
+              <td>Programador JR</td>
+              <td>16 de Mayo del 2022</td>
+              <td>${employee.payment}</td>
+              <td>
+                <span>Ver información</span>
+                <span>Editar empleado</span>
+              </td>
+            </tr>
+          ))}
+        <Paginator
+          arr={employees}
+          changePage={setPagination}
+          currentPage={pagination}
+          numberItemsPerPage={10}
+        />
       </CustomTable>
     </Fragment>
   );
--- frontend-next/pages/dashboard/products/components/dashboard.listProducts.tsx
+++ frontend-next/pages/dashboard/products/components/dashboard.listProducts.tsx
@@ -1,9 +1,11 @@
+import { Paginator } from '@/components/paginator/paginator';
 import { CustomTable } from '@/components/tables/table.component';
 import Link from 'next/link';
 import { Product } from 'pages/home/adapters/product.adapter';
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 export const ListProducts = ({ products }: { products: Product[] }) => {
+  const [pagination, setPagination] = useState<number>(0);
   return (
     <Fragment>
       <CustomTable
@@ -28,24 +30,30 @@ export const ListProducts = ({ products }: { products: Product[] }) => {
           },
         ]}
       >
-        {products.map((product: Product) => (
-        <tr key={product._id}>
-          <td>{product.title}</td>
-          <td>{product.category}</td>
-          <td>${product.price}</td>
-          <td>{product.discount}%</td>
-          <td>{product.description}</td>
-          <td>
-            {/* TODO: Cuando este lista la vista de Producto de client, agregarlo. */}
-            <Link href={``}>
-              <button>Ver producto en tienda</button>
-            </Link>
-            <Link href={`products/${product._id}`}>
-              <button>Editar producto</button>
-            </Link>
-          </td>
-        </tr>
-      ))}
+        {products.slice(pagination, pagination + 10).map((product: Product) => (
+          <tr key={product._id}>
+            <td>{product.title}</td>
+            <td>{product.category}</td>
+            <td>${product.price}</td>
+            <td>{product.discount}%</td>
+            <td>{product.description}</td>
+            <td>
+              {/* TODO: Cuando este lista la vista de Producto de client, agregarlo. */}
+              <Link href={``}>
+                <span>Ver producto en tienda</span>
+              </Link>
+              <Link href={`products/${product._id}`}>
+                <span>Editar producto</span>
+              </Link>
+            </td>
+          </tr>
+        ))}
+        <Paginator
+          arr={products}
+          changePage={setPagination}
+          currentPage={pagination}
+          numberItemsPerPage={10}
+        />
       </CustomTable>
     </Fragment>
   );
